d45061b413f90a3f7db236e5a5bafc6ff7535365
rhinehardtc <rhinehardtc@gmail.com>
2020-05-19 21:32:43 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,14 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  def create
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
